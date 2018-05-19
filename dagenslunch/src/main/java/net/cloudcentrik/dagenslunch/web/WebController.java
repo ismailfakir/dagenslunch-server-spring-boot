c4ecbbba418e47685a440c90f9fe8fe93b591022
Ismail Fakir <md.ismail.fakir@gmail.com>
2018-05-20 00:42:37 +0200

    Add restaurant form added

--- dagenslunch/src/main/java/net/cloudcentrik/dagenslunch/web/WebController.java
+++ dagenslunch/src/main/java/net/cloudcentrik/dagenslunch/web/WebController.java
@@ -2,36 +2,32 @@ package net.cloudcentrik.dagenslunch.web;
 
 import net.cloudcentrik.dagenslunch.restaurant.Restaurant;
 import net.cloudcentrik.dagenslunch.restaurant.RestaurantService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
 @ControllerAdvice
 @Controller
-@RequestMapping("/")
-public class WebController {
+public class WebController implements ErrorController {
+
+    private static final Logger log = LoggerFactory.getLogger(WebController.class);
 
     @Autowired
     RestaurantService restaurantService;
 
     @GetMapping(value = "/")
-    public String welcome(Model model) {
-        model.addAttribute("restaurantList",restaurantService.getAllRestaurant());
+    public String index(Model model) {
         return "index";
     }
 
-    @GetMapping(value = "/tables")
-    public String forms(Model model) {
-        model.addAttribute("restaurantList",restaurantService.getAllRestaurant());
-        return "tables";
-    }
-
-    @GetMapping(value = "/home")
-    public String home(Model model) {
-        model.addAttribute("restaurantList",restaurantService.getAllRestaurant());
-        return "home";
-    }
 
     @GetMapping(value = "/restaurantlist")
     public String restaurantList(Model model) {
@@ -47,6 +43,32 @@ public class WebController {
 
     @PostMapping(value= "/addrestaurant")
     public String addRestaurant(@ModelAttribute Restaurant restaurant) {
-        return "redirect: /home";
+        log.info(restaurant.getName());
+        restaurantService.saveRestaurant(restaurant);
+        return "redirect:restaurantlist";
     }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "404";
+            }
+            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "500";
+            }
+        }
+        return "error";
+    }
+
+
 }
